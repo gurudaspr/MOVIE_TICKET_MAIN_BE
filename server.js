bb63fc0bf205c4ddf5b046433e2ecb2a53ee9a07
gurudaspr <gurudaspranavam007@gmail.com>
2024-05-26 20:28:38 +0530

    protect-route check

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const PORT = process.env.PORT || 7895;
 const app = express();
 app.use(cors({
-    origin:' https://movie-ticket-user-fe.vercel.app/',
+    origin:'https://movie-ticket-user-fe.vercel.app/',
     credentials: true,
   }));
 
