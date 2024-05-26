6b88567086d7f09ca5c7b0f3d941c2dc3bc2f5a1
gurudaspr <gurudaspranavam007@gmail.com>
2024-05-26 13:02:06 +0530

    protect-route check

--- middlewares/user.middleware.js
+++ middlewares/user.middleware.js
@@ -5,6 +5,9 @@ dotenv.config();
 
 function authenticateUser(req, res, next) {
   const token = req.cookies.token;
+  if(!token){
+    return res.status(403).send('token not found');
+  }
 jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     console.log('user-middle',err);
 
