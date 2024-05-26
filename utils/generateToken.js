abf1e53803bcd2756eeec20590c10b19f8553984
gurudaspr <gurudaspranavam007@gmail.com>
2024-05-26 20:08:14 +0530

    protect-route check

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -6,7 +6,7 @@ dotenv.config();
 const secret_key = process.env.JWT_SECRET;
 
 export const generateToken = (email) => {
-  return jwt.sign({ data: email }, secret_key, { expiresIn: "1d",secure: true});
+  return jwt.sign({ data: email }, secret_key, { expiresIn: "15d",});
 };
 
 export const adminToken = (user) => {
