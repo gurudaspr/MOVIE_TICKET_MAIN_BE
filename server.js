efbf18d3bcd3631814bbf0dee2c5001d8ba11132
gurudaspr <gurudaspranavam007@gmail.com>
2024-05-26 11:13:07 +0530

    upto seating completed

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ import adminRoutes from './routes/admin.routes.js';
 dotenv.config();
 const PORT = process.env.PORT || 7895;
 const app = express();
-app.use(cors({ origin: true, credentials: true
+app.use(cors({ origin: 'http://localhost:5173', credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
