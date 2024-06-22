11fccdc4d35f156fd68a63f5e493d7b725daf484
gurudaspr <gurudaspranavam007@gmail.com>
2024-06-22 15:38:42 +0530

    edited res cookie

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -57,7 +57,7 @@ export const Signin = async (req, res) => {
     res.cookie("token", token, {
       maxAge: 1 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "lax",
+      sameSite: "none",
       secure: process.env.NODE_ENV !== "development",
       domain: "movie-ticket-user-fe.vercel.app" 
     });
