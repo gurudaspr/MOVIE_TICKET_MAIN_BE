1319f4f9aef28bdaf831e4aca004c8c0321bf601
gurudaspr <gurudaspranavam007@gmail.com>
2024-07-03 20:52:44 +0530

    added cors

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
+  withCredentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
